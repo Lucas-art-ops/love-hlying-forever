fb09a471cafa98665d00c8ed7e84ef7f16b81ea0
Lucas-art-ops <83206320+Lucas-art-ops@users.noreply.github.com>
2021-06-19 14:43:00 +0800

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char[] chars=new char[]{'a','c','u','b','e','p','f','z'};
+        System.out.print("原字符序列：");
+        for (char oldChar:chars){
+            System.out.print(oldChar +" ");
+        }
+        Arrays.sort(chars);
+        System.out.print("\n排序后的字符序列：");
+        for(char oldChar:chars){
+            System.out.print(oldChar +" ");
+        }
+        int index=Arrays.binarySearch(chars,'b');
+        System.out.println("字符b在数组中的位置是"+index);
+    }
+}
