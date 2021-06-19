01f5d26e0dba5873815ed8230420be42f999ecce
Eric <Eric@guigu.com>
2021-06-19 16:05:04 +0800

    第二次修改

--- CharDemo.java
+++ CharDemo.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class CharDemo {
     public static void main(String[] args) {
         char[] chars=new char[]{'a','c','u','b','e','p','f','z'};
-        System.out.print("原字符序列：");
+        System.out.print("字符序列：");
         for (char oldChar:chars){
             System.out.print(oldChar +" ");
         }
